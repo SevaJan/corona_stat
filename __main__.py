cb8aa09e5982c3816134ba46c6c92069a982eab9
Vsevolod Podshibyakin <vsevolodpodshibyakin@Vsevolods-MacBook-Air.local>
2020-04-29 21:10:16 +0300

    exchange + new stopcoronavirus

--- __main__.py
+++ __main__.py
@@ -1,17 +1,27 @@
 import datetime
 import requests
 from bs4 import BeautifulSoup
+import pycbrf
+import datetime
+import json
 
 def get_statistics(parameters):
-    country = parameters.get('country')
-    region = parameters.get('region')
+    world = str(parameters.get('world'))
+    russia = str(parameters.get('russia'))
+    type_of_currency = str(parameters.get('testinput'))
 
-    if str(country) != 'None':
-        return worldometers_info(country)
-    elif region == 'Russia':
-        return worldometers_info(region)
+    #world = 'Vatican City'
+    #russia = 'None'
+    #type_of_currency = 'None'
+
+    if type_of_currency != 'None':
+        return get_rate(type_of_currency)
+    elif world != 'None':
+        return worldometers_info(world)
+    elif russia == 'Russia':
+        return worldometers_info(russia)
     else:
-        return stopcoronavirus_rf(region)
+        return stopcoronavirus_rf(russia)
 
 def worldometers_info(location):
     worldmetersLink = 'https://www.worldometers.info/coronavirus/'
@@ -22,31 +32,31 @@ def worldometers_info(location):
             i = i.replace('+', '')
             i = i.replace('-', '')
             i = i.replace(',', '')
-        
+            
             if i == '':
                 i = '0'
-            
+                
             L.append(i.strip())
-        
+            
         return L
 
     try:
         html_page = requests.get(worldmetersLink)
     except requests.exceptions.RequestException as e: 
         print (e)
-    
+        
     bs = BeautifulSoup(html_page.content, 'html.parser')
     search_day = bs.find('div', {'id': 'nav-yesterday'})
     search = search_day.select('tbody tr td')
     start = -1
-    
+        
     for i in range(len(search)):
         if search[i].get_text().find(location) != -1:
             start = i
             break
-    
+        
     data = []
-    
+        
     for i in range(1, 8):
         try:
             data = data + [search[start + i].get_text()]
@@ -54,20 +64,20 @@ def worldometers_info(location):
             data = data + ['0']
 
     data_y = data_cleanup(data)
-    new_y = data_y[1]
-    death_y = data_y[3]
+
+    y_recovered = data_y[4]
 
     search_day = bs.find('div', {'id': 'nav-today'})
     search = search_day.select('tbody tr td')
     start = -1
-    
+        
     for i in range(len(search)):
         if search[i].get_text().find(location) != -1:
             start = i
             break
-    
+        
     data = []
-    
+        
     for i in range(1, 8):
         try:
             data = data + [search[start + i].get_text()]
@@ -75,49 +85,106 @@ def worldometers_info(location):
             data = data + ['0']
 
     data = data_cleanup(data)
-    today = datetime.date.today()
-    yesterday = today - datetime.timedelta(days = 1)
-    notation = 'Информация за день сбрасывается и обновляется после полуночи по Гринвичу (UTC ±0:00)'
-    res = '\n\nСтатистика за {}:\n• Всего заболевших — {},\n• Новые случаи — {},\n• Всего умерших — {},\n• Новые смерти — {},\n• Вылечились — {},\n• Болеют на данный момент — {},\n• В критическом состоянии — {}.\n\nСтатистика за {}:\n• Новые случаи — {},\n• Новые смерти — {}.\n\n{}.'.format(today, *data, yesterday, new_y, death_y, notation)
+
+    t_sick = data[0]
+    t_sick_new = data[1]
+    t_recovered = data[4]
+    if t_recovered == 'N/A':
+        t_recovered = 'Неизвестно'
+    if t_recovered == 'Неизвестно' or y_recovered == 'N/A':
+        t_recovered_new = 'Неизвестно'
+    else:
+        t_recovered_new = int(t_recovered) - int(y_recovered)
+    t_died = data[2]
+    t_died_new = data[3]
+
+    search_date = str(bs.find_all('div'))
+    start_date = search_date.find('<div style="font-size:13px; color:#999; margin-top:5px; text-align:center">Last updated: ') + len('<div style="font-size:13px; color:#999; margin-top:5px; text-align:center">Last updated: ')
+    date = ''
+
+    while search_date[start_date] != '<':
+        date += search_date[start_date]
+        start_date += 1
+
+    if t_sick == '':
+        t_sick = 0
+    if t_sick_new == '':
+        t_sick_new = 0
+    if t_recovered == '':
+        t_recovered = 0
+    if t_recovered_new == '':
+        t_recovered_new = 0
+    if t_died == '':
+        t_died = 0
+    if t_died_new == '':
+        t_died_new = 0
     
-    return ({'result' : str(res)})
+    if t_recovered == 'Неизвестно':
+        t_sick, t_sick_new, t_died, t_died_new = "{:,}".format(int(t_sick)), "{:,}".format(int(t_sick_new)), "{:,}".format(int(t_died)), "{:,}".format(int(t_died_new))
+    elif t_recovered != 'Неизвестно' and t_recovered_new == 'Неизвестно':
+        t_sick, t_sick_new, t_recovered, t_died, t_died_new = "{:,}".format(int(t_sick)), "{:,}".format(int(t_sick_new)), "{:,}".format(int(t_recovered)), "{:,}".format(int(t_died)), "{:,}".format(int(t_died_new))
+    else:   
+        t_sick, t_sick_new, t_recovered, t_recovered_new, t_died, t_died_new = "{:,}".format(int(t_sick)), "{:,}".format(int(t_sick_new)), "{:,}".format(int(t_recovered)), "{:,}".format(int(t_recovered_new)), "{:,}".format(int(t_died)), "{:,}".format(int(t_died_new))
+
+    res = date + '\nПоследние данные по коронавирусу:\n\nЗа всё время / за сутки\n<b>' + location + '</b>\nЗаболели: ' + t_sick + ' / ' + t_sick_new + '\nВылечились: ' + t_recovered + ' / ' + t_recovered_new + '\nУмерли: ' + t_died + ' / ' + t_died_new
+
+    return ({'result' : res})
 
 def stopcoronavirus_rf(location):
-    stop_coronavirus = 'https://xn--80aesfpebagmfblc0a.xn--p1ai/'
+    stop_coronavirus = 'https://xn--80aesfpebagmfblc0a.xn--p1ai/information/'
 
     try:
         html_page = requests.get(stop_coronavirus)
     except requests.exceptions.RequestException as e: 
         print (e)
-    
+        
     bs = BeautifulSoup(html_page.content, 'html.parser')
-    search_regions = bs.find_all('tr')
-    search_regions_str = str(search_regions)
+    search_regions = str(bs.find_all('cv-spread-overview'))
+    start_global = search_regions.find(location)
 
-    start_global = search_regions_str.find(location)
-    start_sick = search_regions_str.find('<span class="d-map__indicator d-map__indicator_sick"></span>', start_global) + len('<span class="d-map__indicator d-map__indicator_sick"></span>')
-    start_healed = search_regions_str.find('<span class="d-map__indicator d-map__indicator_healed"></span>', start_global) + len('<span class="d-map__indicator d-map__indicator_healed"></span>')
-    start_die = search_regions_str.find('<span class="d-map__indicator d-map__indicator_die"></span>', start_global) + len('<span class="d-map__indicator d-map__indicator_die"></span>')
-    
-    sick = healed = die = ''
+    start_sick = search_regions.find('"sick":', start_global) + len('"sick":')
+    start_healed = search_regions.find('"healed":', start_global) + len('"healed":')
+    start_died = search_regions.find('"died":', start_global) + len('"died":')
+    start_sick_incr = search_regions.find('"sick_incr":', start_global) + len('"sick_incr":')
+    start_healed_incr = search_regions.find('"healed_incr":', start_global) + len('"healed_incr":')
+    start_died_incr = search_regions.find('"died_incr":', start_global) + len('"died_incr":')
 
-    while search_regions_str[start_sick] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-        sick += search_regions_str[start_sick]
+    sick = healed = died = sick_incr = healed_incr = died_incr = ''
+
+    while search_regions[start_sick] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        sick += search_regions[start_sick]
         start_sick += 1
 
-    while search_regions_str[start_healed] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-        healed += search_regions_str[start_healed]
+    while search_regions[start_healed] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        healed += search_regions[start_healed]
         start_healed += 1
 
-    while search_regions_str[start_die] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-        die += search_regions_str[start_die]
-        start_die += 1
+    while search_regions[start_died] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        died += search_regions[start_died]
+        start_died += 1
 
-    search_date = bs.find('div', {'class': 'd-map__title'})
-    search_date_str = str(search_date)
-    search_date_str_clear = search_date_str.replace('<div class="d-map__title"><h2>', '')
-    search_date_str_clear = search_date_str_clear.replace('<span>', ' ')
-    search_date_str_clear = search_date_str_clear.replace('</span></h2></div>', ':')
-    res = '\n\n' + search_date_str_clear + '\n' + '• Случаев заболевания — ' + sick + ',\n' + '• Человек выздоровело — ' + healed + ',\n' + '• Человек умерло — ' + die + '.'
+    while search_regions[start_sick_incr] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        sick_incr += search_regions[start_sick_incr]
+        start_sick_incr += 1
+
+    while search_regions[start_healed_incr] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        healed_incr += search_regions[start_healed_incr]
+        start_healed_incr += 1
+
+    while search_regions[start_died_incr] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        died_incr += search_regions[start_died_incr]
+        start_died_incr += 1
+
+    sick, healed, died, sick_incr, healed_incr, died_incr = "{:,}".format(int(sick)), "{:,}".format(int(healed)), "{:,}".format(int(died)), "{:,}".format(int(sick_incr)), "{:,}".format(int(healed_incr)), "{:,}".format(int(died_incr))
+
+    search_date = str(bs.find_all('small'))
+    search_date = search_date.replace('[<small>По состоянию на ', '')
+    search_date = search_date.replace('</small>]', '')
+
+    res = search_date + '\nПоследние данные по коронавирусу:\n\nЗа всё время / за сутки\n<b>' + location + '</b>\nЗаболели: ' + sick + ' / ' + sick_incr + '\nВылечились: ' + healed + ' / ' + healed_incr + '\nУмерли: ' + died + ' / ' + died_incr
 
     return ({'result' : res})
+
+def get_rate(type_of_currency):
+    today_date = datetime.date.today()
+    return {'result' : str(pycbrf.ExchangeRates(today_date)[type_of_currency].rate)}
--- date.py
+++ date.py
@@ -1,26 +1,89 @@
-from bs4 import BeautifulSoup
-import requests
 import datetime
+import requests
+from bs4 import BeautifulSoup
 
+worldmetersLink = 'https://www.worldometers.info/coronavirus/'
+location = 'World'
 
-stop_coronavirus = 'https://xn--80aesfpebagmfblc0a.xn--p1ai/'
+def data_cleanup(array):
+    L = []
+    for i in array:
+        i = i.replace('+', '')
+        i = i.replace('-', '')
+        i = i.replace(',', '')
+        
+        if i == '':
+            i = '0'
+            
+        L.append(i.strip())
+        
+    return L
 
 try:
-    html_page = requests.get(stop_coronavirus)
+    html_page = requests.get(worldmetersLink)
 except requests.exceptions.RequestException as e: 
     print (e)
+    
 bs = BeautifulSoup(html_page.content, 'html.parser')
+search_day = bs.find('div', {'id': 'nav-yesterday'})
+search = search_day.select('tbody tr td')
+start = -1
+    
+for i in range(len(search)):
+    if search[i].get_text().find(location) != -1:
+        start = i
+        break
+    
+data = []
+    
+for i in range(1, 8):
+    try:
+        data = data + [search[start + i].get_text()]
+    except:
+        data = data + ['0']
 
-search_date = bs.find('div', {'class': 'd-map__title'})
-search_date_str = str(search_date)
-search_date_str_clear = search_date_str.replace('<div class="d-map__title"><h2>', '')
-search_date_str_clear = search_date_str_clear.replace('<span>', ' ')
-search_date_str_clear = search_date_str_clear.replace('</span></h2></div>', ':')
+data_y = data_cleanup(data)
 
-dict = {'Name': 'AndreyEx', 'Age': 18}
-res = str(dict.get('country'))
+y_recovered = data_y[4]
+
+search_day = bs.find('div', {'id': 'nav-today'})
+search = search_day.select('tbody tr td')
+start = -1
+    
+for i in range(len(search)):
+    if search[i].get_text().find(location) != -1:
+        start = i
+        break
+    
+data = []
+    
+for i in range(1, 8):
+    try:
+        data = data + [search[start + i].get_text()]
+    except:
+        data = data + ['0']
+
+data = data_cleanup(data)
+
+t_sick = data[0]
+t_sick_new = data[1]
+t_recovered = data[4]
+t_recovered_new = int(t_recovered) - int(y_recovered)
+t_died = data[2]
+t_died_new = data[3]
+
+search_date = str(bs.find_all('div'))
+start_date = search_date.find('<div style="font-size:13px; color:#999; margin-top:5px; text-align:center">Last updated: ') + len('<div style="font-size:13px; color:#999; margin-top:5px; text-align:center">Last updated: ')
+date = ''
+
+while search_date[start_date] != '<':
+    date += search_date[start_date]
+    start_date += 1
+
+t_sick, t_sick_new, t_recovered, t_recovered_new, t_died, t_died_new = "{:,}".format(int(t_sick)), "{:,}".format(int(t_sick_new)), "{:,}".format(int(t_recovered)), "{:,}".format(int(t_recovered_new)), "{:,}".format(int(t_died)), "{:,}".format(int(t_died_new))
+
+res = date + '\nПоследние данные по коронавирусу:\n\nЗа всё время / за сутки\n<b>' + location + '<b>\nЗаболели: ' + t_sick + ' / ' + t_sick_new + '\nВылечились: ' + t_recovered + ' / ' + t_recovered_new + '\nУмерли: ' + t_died + ' / ' + t_died_new
+
 print(res)
 
-#res = '\n\n' + search_date_str_clear
-
-#print(res)
+#return ({'result' : res})
